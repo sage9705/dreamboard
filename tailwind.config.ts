0918f39502eea11599e8e59083cf6d811d4cec97
Sage9705 <edem22kumahor@gmail.com>
2024-07-22 17:35:04 +0100

    Add colours

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,12 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
@@ -13,8 +15,14 @@ module.exports = {
           light: '#4DB6AC',
         },
         secondary: '#FF7F50',
-        background: '#F8F9FA',
-        text: '#2C3E50',
+        background: {
+          light: '#F8F9FA',
+          dark: '#2C3E50',
+        },
+        text: {
+          light: '#2C3E50',
+          dark: '#F8F9FA',
+        },
         accent: {
           yellow: '#FFF59D',
           lavender: '#E1BEE7',
@@ -24,3 +32,5 @@ module.exports = {
   },
   plugins: [],
 }
+
+export default config
